a85bd945b7fb54d39900e1410cb8a6453a9a2efa
kowu0 <kowu010@gmail.com>
2025-03-24 14:31:16 +0100

    1&2 exercise

--- zad1.cs
+++ zad1.cs
@@ -1,1 +1,13 @@
 ﻿//ustaw wartości w tablicy
+
+var numbers = new int[3];
+
+numbers[0] = 10;
+numbers[1] = 5;
+numbers[2] = 1;
+
+
+//wypisz elementy z tablicy
+foreach(int x in numbers){
+    System.Console.WriteLine(x);
+}
